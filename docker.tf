f67ca5f22adcc59a125cc5976e7276b96ac6c392
BharathKumarReddy <nbkumar2103@gmail.com>
2025-07-23 19:04:14 +0530

    updated file

--- docker.tf
+++ docker.tf
@@ -20,6 +20,7 @@ resource "aws_instance" "docker" {
   ami           = local.ami_id
   #instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
+  associate_public_ip_address = true
   instance_type = "t3.medium"
   subnet_id     = data.aws_subnet.default.id  
   # need more for terraform
