cff60f7b034417d15bd778dc5a5230d5e44fc3b7
BharathKumarReddy <nbkumar2103@gmail.com>
2025-07-23 19:10:56 +0530

    updated file

--- docker.sh
+++ docker.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-dnf -y install dnf-plugins-core
+# Log to file
+exec > /var/log/user-data.log 2>&1
+set -xe
+
+# Install required tools
+dnf -y install dnf-plugins-core curl
+
+# Add Docker CE repo
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+dnf config-manager --set-enabled docker-ce-stable
+
+# Install Docker
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Start and enable Docker
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
+echo 'newgrp docker' >> /home/ec2-user/.bashrc
 
 growpart /dev/nvme0n1 4
 lvextend -L +20G /dev/RootVG/rootVol
